ae7cb37532fa1e75ee4122e3f91892f09fa90aeb
Abdullahcan Karki <abdullahcankarki@MBPvonAllahcan5.fritz.box>
2025-07-03 12:30:50 +0200

    SUpermarkt Kategorie raus

--- src/components/kundenaufpreiseditor.tsx
+++ src/components/kundenaufpreiseditor.tsx
@@ -282,7 +282,6 @@ const saveMassSurcharges = async () => {
                     <option value="">Kategorie wählen</option>
                     <option value="Discounter">Discounter</option>
                     <option value="Gastronomie">Gastronomie</option>
-                    <option value="Supermarkt">Supermarkt</option>
                     <option value="Großhandel">Großhandel</option>
                     <option value="custom">Andere (manuell eingeben)</option>
                   </select>
